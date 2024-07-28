b230103738cac006d655fd34386901283d12e2a6
Rob Blanckaert <basicer@basicer.com>
2024-07-28 00:38:55 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# v86-pkg
+
+Add `"v86": "basicer/v86-pkg"` to your package.json file to get the latest v86.
---
+++ prepare.mjs
@@ -0,0 +1,26 @@
+import {writeFile, existsSync} from 'node:fs';
+
+const files = {
+    "libv86.js": "https://github.com/copy/v86/releases/download/latest/libv86.js",
+    "libv86-debug.js": "https://github.com/copy/v86/releases/download/latest/libv86-debug.js",
+
+    "v86.wasm": "https://github.com/copy/v86/releases/download/latest/v86.wasm",
+    "v86-debug.wasm": "https://github.com/copy/v86/releases/download/latest/v86-debug.wasm",
+    "v86-fallback.wasm": "https://github.com/copy/v86/releases/download/latest/v86-fallback.wasm",
+
+    "seabios.bin": "https://raw.githubusercontent.com/copy/v86/master/bios/seabios.bin",
+};
+
+await Promise.all(Object.entries(files).map(async ([name, url]) => {
+    if (existsSync(name)) return false;
+    let res = await fetch(url);
+    let data = await res.arrayBuffer();
+    await new Promise((res, rej) => {
+        writeFile(name, Buffer.from(data), (err) => {
+            if (err) rej(err);
+            else res();
+        });
+    });
+}));
+
+console.log("Done!");
